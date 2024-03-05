985b9aac759c70a34ea0fa779e07a34907d6e8c0
James Smith <jismith1989@googlemail.com>
2024-03-05 02:38:21 +0000

    Create epub file

---
+++ epub.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"image"
+	"os"
+)
+
+const (
+	epubInfoFileExt  = ".json"
+	epubInfoFileName = "epub_info" + epubInfoFileExt
+)
+
+type epubInfo struct {
+	ISBN   string `json:"isbn"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+	Paths  struct {
+		CoverImage string `json:"cover_image"`
+		Styles     string `json:"styles"`
+		Text       string `json:"text"`
+	} `json:"paths"`
+
+	coverImage       image.Image
+	coverImageFormat string
+}
+
+type epubInfoInitHandler = func(*epubInfo) error
+
+var (
+	epubInfoInitHandlerList = []epubInfoInitHandler{
+		epubInfoInitCoverImage,
+	}
+)
+
+func epubInfoInit(ei *epubInfo) {
+	for _, handler := range epubInfoInitHandlerList {
+		handler(ei)
+	}
+}
+
+func epubInfoInitCoverImage(ei *epubInfo) (err error) {
+	if ei.Paths.CoverImage == "" {
+		return
+	}
+
+	f, err := os.Open(ei.Paths.CoverImage)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+
+	image, imageFormat, err := image.Decode(f)
+	if err != nil {
+		return
+	}
+
+	ei.coverImage = image
+	ei.coverImageFormat = imageFormat
+
+	return
+}
--- main.go
+++ main.go
@@ -2,24 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	_ "image/png"
 	"os"
 )
-
-const (
-	epubInfoFileExt  = ".json"
-	epubInfoFileName = "epub_info" + epubInfoFileExt
-)
-
-type epubInfo struct {
-	ISBN   string `json:"isbn"`
-	Title  string `json:"title"`
-	Author string `json:"author"`
-	Paths  struct {
-		CoverImage string `json:"cover_image"`
-		Styles     string `json:"styles"`
-		Text       string `json:"text"`
-	} `json:"paths"`
-}
 
 func main() {
 	fileContent, err := os.ReadFile(epubInfoFileName)
@@ -33,6 +18,8 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	epubInfoInit(&ei)
 
 	if err := generate(&ei); err != nil {
 		panic(err)
